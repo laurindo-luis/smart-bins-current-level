aeded69d912fc339a5edcb390b5a85291fa012b4
Luis Eduardo <luiseduardocosta417@gmail.com>
2021-09-21 09:16:13 -0300

    first commit

---
+++ src/main/java/br/com/luis/SmartBinsCurrentLevelApplication.java
@@ -0,0 +1,13 @@
+package br.com.luis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBinsCurrentLevelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBinsCurrentLevelApplication.class, args);
+	}
+
+}
