76a047451ab940b324fb94e3be85be51b399832c
Luis Eduardo <luiseduardocosta417@gmail.com>
2021-09-21 11:24:00 -0300

    task scheduler

--- src/main/java/br/com/luis/SmartBinsCurrentLevelApplication.java
+++ src/main/java/br/com/luis/SmartBinsCurrentLevelApplication.java
@@ -1,5 +1,9 @@
 package br.com.luis;
 
+import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -19,7 +23,16 @@ public class SmartBinsCurrentLevelApplication {
 	@Bean
 	public CommandLineRunner run() {
 		return args -> {
-			smartBinsLevelService.save();
+			
+			long minute = 60;
+			new Timer().scheduleAtFixedRate(new TimerTask() {
+				public void run() {
+					LocalDateTime dateTime = LocalDateTime.now();
+					System.out.print("Saving data -> "+dateTime);
+					
+					smartBinsLevelService.save();
+				}
+			}, 0, minute * 60000);
 		};
 	}
 }
