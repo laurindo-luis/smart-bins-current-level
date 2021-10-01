60ca3ce69cdb1c47bd8de90956fbc2fa4f0053f7
Luis Eduardo <luiseduardocosta417@gmail.com>
2021-10-01 11:51:18 -0300

    ResourceAccessException

--- src/main/java/br/com/luis/SmartBinsCurrentLevelApplication.java
+++ src/main/java/br/com/luis/SmartBinsCurrentLevelApplication.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,6 +19,9 @@ public class SmartBinsCurrentLevelApplication {
 	@Autowired
 	private SmartBinsLevelService smartBinsLevelService;
 	
+	private Logger logger = LoggerFactory.getLogger(SmartBinsLevelService.class);
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SmartBinsCurrentLevelApplication.class, args);
 	}
@@ -24,12 +29,12 @@ public class SmartBinsCurrentLevelApplication {
 	@Bean
 	public CommandLineRunner run() {
 		return args -> {
-			long minute = 60;
+			long minute = 1;
 			new Timer().scheduleAtFixedRate(new TimerTask() {
 				public void run() {					
 					HttpStatus status = smartBinsLevelService.save();
 					LocalDateTime dateTime = LocalDateTime.now();
-					System.out.print(String.format("Task scheduler has run -> %s | Status Http: %s", 
+					logger.info(String.format("Task scheduler has run -> %s | Status Http: %s", 
 							dateTime, status));
 				}
 			}, 0, minute * 60000);
--- src/main/java/br/com/luis/SmartBinsLevelService.java
+++ src/main/java/br/com/luis/SmartBinsLevelService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -17,18 +18,21 @@ public class SmartBinsLevelService {
 	
 	private ResponseEntity<SmartBinsLevelDto> get() {
 		RestTemplate rest = new RestTemplate();
-		ResponseEntity<SmartBinsLevelDto> response = rest.getForEntity("https://data.randwick.nsw.gov.au/api/"
-				+ "records/1.0/search/?dataset=smart-bins-current-level&q=&rows=100&sort=timestamp&"
-				+ "timezone=America/Argentina/Buenos_Aires&facet=bin_id&facet=bin_status", 
-				SmartBinsLevelDto.class);
-		return response;
+		try {
+			ResponseEntity<SmartBinsLevelDto> response = rest.getForEntity("https://data.randwick.nsw.gov.au/api/"
+					+ "records/1.0/search/?dataset=smart-bins-current-level&q=&rows=100&sort=timestamp&"
+					+ "timezone=America/Argentina/Buenos_Aires&facet=bin_id&facet=bin_status", 
+					SmartBinsLevelDto.class);
+			return response;
+		} catch(ResourceAccessException e) {
+			return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(null);
+		}
 	}
 	
 	public HttpStatus save() {
 		ResponseEntity<SmartBinsLevelDto> response = get();
-		SmartBinsLevelDto smartBinsLevel = response.getBody();
-		if(nonNull(smartBinsLevel)) {
-			List<Record> records = smartBinsLevel.getRecords();
+		if(response.getStatusCode().value() == 200) {
+			List<Record> records = response.getBody().getRecords();
 			records.forEach(record -> {
 				Field field = record.getFields();
 				if(nonNull(field)) {
